f62dc99cd8a66bb7919c32f14f5df860e919762c
Aadi639707 <aadi639707@gmail.com>
2025-12-30 13:08:55 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
   function rollDice() {
     const dice = Math.floor(Math.random() * 6) + 1;
-    setRedPos(prev => prev + dice);
+    setRedPos(redPos + dice);
   }
 
   return (
@@ -14,24 +14,7 @@ export default function Home() {
       <button className="dice" onClick={rollDice}>🎲 Roll</button>
 
       <div className="board">
-
-        {/* Top */}
-        <div className="home blue"></div>
-        <div className="path"></div>
-        <div className="home yellow"></div>
-
-        {/* Middle */}
-        <div className="path"></div>
-        <div className="center">
-          <div className="token redToken" style={{transform:translateX(${redPos*20}px)}}></div>
-        </div>
-        <div className="path"></div>
-
-        {/* Bottom */}
-        <div className="home red"></div>
-        <div className="path"></div>
-        <div className="home green"></div>
-
+        <div className="token redToken" style={{ left: redPos * 30 }}></div>
       </div>
     </div>
   );
