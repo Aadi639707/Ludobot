464d0391b2e8a89ed0de1aa779d6ca415c06e014
Aadi639707 <aadi639707@gmail.com>
2025-12-30 11:16:35 +0530

    Create Home component for main page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>🎲 LudoBot</h1>
+      <p>Site is running ✅</p>
+    </main>
+  );
+}
