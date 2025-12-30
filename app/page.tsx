4199ad1c8b928d5d7cc1add21f8542f636772d2f
Aadi639707 <aadi639707@gmail.com>
2025-12-30 12:51:10 +0530

    Refactor Home component layout structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
-"use client";
-
 export default function Home() {
   return (
-    <main className="screen">
+    <div className="screen">
       <div className="board">
-        <div className="tile">Test</div>
+
+        {/* Blue Home */}
+        <div className="cell blue" style={{ gridColumn: "1/7", gridRow: "1/7" }} />
+
+        {/* Yellow Home */}
+        <div className="cell yellow" style={{ gridColumn: "10/16", gridRow: "1/7" }} />
+
+        {/* Red Home */}
+        <div className="cell red" style={{ gridColumn: "1/7", gridRow: "10/16" }} />
+
+        {/* Green Home */}
+        <div className="cell green" style={{ gridColumn: "10/16", gridRow: "10/16" }} />
+
+        {/* Center */}
+        <div className="center" />
+
       </div>
-    </main>
+    </div>
   );
 }
